ed1f6832d8cd48d22df388b2416e488f3ad9d854
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-10-10 18:56:11 +0530

    Create 3147-TakingMaximumEnergyFromtheMysticDungeon

---
+++ 3147-TakingMaximumEnergyFromtheMysticDungeon/version/java/3147-TakingMaximumEnergyFromtheMysticDungeon_20251010_185610.java
@@ -0,0 +1,18 @@
+// Last updated: 10/10/2025, 6:56:10 PM
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int n = energy.length;
+        int[] dp = new int[n];
+        int maxEnergy = Integer.MIN_VALUE;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = energy[i];
+            if (i + k < n) {
+                dp[i] += dp[i + k];
+            }
+            maxEnergy = Math.max(maxEnergy, dp[i]);
+        }
+        
+        return maxEnergy;
+    }
+}
