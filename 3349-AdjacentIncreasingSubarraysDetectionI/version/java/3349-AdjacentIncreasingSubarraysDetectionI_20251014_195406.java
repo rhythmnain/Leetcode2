0becafbff1025761f10b384f5fae049ae5bf4404
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-10-14 19:54:07 +0530

    Create 3349-AdjacentIncreasingSubarraysDetectionI

---
+++ 3349-AdjacentIncreasingSubarraysDetectionI/version/java/3349-AdjacentIncreasingSubarraysDetectionI_20251014_195406.java
@@ -0,0 +1,16 @@
+// Last updated: 10/14/2025, 7:54:06 PM
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int knew = k - 1;
+        if (knew == 0) return true;
+        for (int j = k + 1; j < nums.size(); j++) {
+            if (nums.get(j) > nums.get(j - 1) && nums.get(j - k) > nums.get(j - k - 1)) {
+                knew--;
+            } else {
+                knew = k - 1;
+            }
+            if (knew == 0) return true;
+        }
+        return false;
+    }
+}
