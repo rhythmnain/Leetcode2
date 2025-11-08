9fbb60f4fcca5ed358ad56bfacb19023ad19489e
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-11-08 13:38:19 +0530

    Create 1611-MinimumOneBitOperationstoMakeIntegersZero

---
+++ 1611-MinimumOneBitOperationstoMakeIntegersZero/version/java/1611-MinimumOneBitOperationstoMakeIntegersZero_20251108_133822.java
@@ -0,0 +1,11 @@
+// Last updated: 11/8/2025, 1:38:22 PM
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+}
